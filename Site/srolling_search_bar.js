e42d1aa4cb1d87841c7196da1af1b7c156566a63
Alghard <Julien.varela@gmail.com>
2024-02-11 20:30:14 +0100

    search-bar script update

--- Site/srolling_search_bar.js
+++ Site/srolling_search_bar.js
@@ -2,23 +2,22 @@ const search = document.querySelector("#search-bar");
 const header = document.querySelector("header");
 let isFixed = false; // Variable pour suivre si la barre est fixe
 function scrolled() {
-    const windowHeight = window.innerHeight;
-    const currentScroll = document.body.scrollTop || document.documentElement.scrollTop;
-
-    // Récupérer la position de l'en-tête par rapport au haut de la page
-    const searchTop = search.getBoundingClientRect().top;
-
     // Condition modifiée pour déclencher le changement de classe dès que l'en-tête disparaît de l'écran
-    if (searchTop <= 0 && !isFixed) {
+    if (scrollY >= 305 && !isFixed) {
         search.classList.add("fixed");
         search.classList.remove("original");
         isFixed = true;
-    } else if (currentScroll > 3 && isFixed) {
+    } else if (scrollY < 306 && isFixed) {
         search.classList.add("original");
-        isFixed = false;
-    } else if (currentScroll < 3 && isFixed) {
         search.classList.remove("fixed");
-        isFixed = true;
+        isFixed = false;
     }
 }
 addEventListener("scroll", scrolled, false);
+addEventListener("scroll", () => {
+    console.log(`
+    Hauteur page : ${document.body.scrollHeight}
+    Hauteur affichage : ${innerHeight}
+    Scroll Position : ${scrollY}
+    isFixed : ${isFixed}`);
+})
