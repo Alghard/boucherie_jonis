bfb65e708486280750ac8f7e9cc4930661f0122d
Alghard <julien.varela@gmail.com>
2024-02-15 14:47:39 +0100

    m

--- Site/script.js
+++ Site/script.js
@@ -5,11 +5,11 @@ const header = document.querySelector("header");
 let isFixed = false; // Variable pour suivre si la barre est fixe
 function scrolled() {
     // Condition modifiée pour déclencher le changement de classe dès que l'en-tête disparaît de l'écran
-    if (scrollY >= 305 && !isFixed) {
+    if (scrollY >= 150 && !isFixed) {
         search.classList.add("fixed");
         search.classList.remove("original");
         isFixed = true;
-    } else if (scrollY < 306 && isFixed) {
+    } else if (scrollY < 150 && isFixed) {
         search.classList.add("original");
         search.classList.remove("fixed");
         isFixed = false;
