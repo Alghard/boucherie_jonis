a2d60f9f68eeeddd91ac25a1ed39777fa35c3673
Alghard <julien.varela@gmail.com>
2024-02-16 16:22:35 +0100

    add uppage btn and shopbtn

--- Site/script.js
+++ Site/script.js
@@ -2,26 +2,30 @@
 
 const search = document.querySelector("#search-bar");
 const header = document.querySelector("header");
+const headerOffset = header.offsetHeight;
 let isFixed = false; // Variable pour suivre si la barre est fixe
-function scrolled() {
+function scrolledSearchBar() {
     // Condition modifiée pour déclencher le changement de classe dès que l'en-tête disparaît de l'écran
-    if (scrollY >= 150 && !isFixed) {
+    if (scrollY >= headerOffset && !isFixed) {
         search.classList.add("fixed");
         search.classList.remove("original");
         isFixed = true;
-    } else if (scrollY < 150 && isFixed) {
+    } else if (scrollY < headerOffset && isFixed) {
         search.classList.add("original");
         search.classList.remove("fixed");
         isFixed = false;
     }
 }
-addEventListener("scroll", scrolled, false);
-addEventListener("scroll", () => {
-    console.log(`
-    Hauteur page : ${document.body.scrollHeight}
-    Hauteur affichage : ${innerHeight}
-    Scroll Position : ${scrollY}
-    isFixed : ${isFixed}`);
-});
+addEventListener("ontouchmove", scrolledSearchBar, false); //For mobile
+addEventListener("scroll", scrolledSearchBar, false); //For computer
+// addEventListener("scroll", () => {
+//     console.log(`
+//     Hauteur page : ${document.body.scrollHeight}
+//     Hauteur affichage : ${innerHeight}
+//     Scroll Position : ${scrollY}
+//     isFixed : ${isFixed}
+//     headerOffset : ${headerOffset}
+//     searchOffset : ${search.offsetHeight}`);
+// });
 
 //FIN Script de sticky search bar
