9e701fad853de098e45435e14c67bcd3b898371e
Alghard <julien.varela@gmail.com>
2024-03-21 16:21:49 +0100

    up

--- Site/controller_result.php
+++ Site/controller_result.php
@@ -2,28 +2,25 @@
 include 'utils.php';
 include './model/model_article.php';
 session_start();
-
+$result ="";
 if(isset($_POST['submit'])){
     if(isset($_POST["search"]) && !empty($_POST["search"])){
         $search = sanitize($_POST['search']);
         $bdd = connect();
         $article = searchArticle($bdd,$search);
-        $_SESSION['article'] = $article;
         if($article != null){
             //boucle for each qui va parcourir le tableau articles
             foreach($article as $article){
-                echo "<div>
-                <h3>".$article['nom_article']."</h3>
+                $result = "<div><h3>".$article['nom_article']."</h3>
                 <p>Description : ".$article['description_article']."</p>
                 <p>Prix unitaire : ".$article['prix_unit_article']." €</p>
                 </div>";
             }
         //SINON message pas de résultat
         }else{
-            echo "No results SRY";
+            $result = "No results SRY";
         }
     }     
-
 }
 
 
--- Site/vue/view_result.php
+++ Site/vue/view_result.php
@@ -1,6 +1,6 @@
 <section>
     <p>On est bien dans view result</p>
     <?php
-    print_r($receivedResult);
+    echo $result;
     ?>
 </section>
