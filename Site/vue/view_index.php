03b13c2eedf41f2ec397c39442f8acab95523f5b
Alghard <julien.varela@gmail.com>
2024-04-11 14:53:26 +0200

    img

--- Site/vue/view_index.php
+++ Site/vue/view_index.php
@@ -2,13 +2,13 @@
         <a href="controller.php">Accueil/</a>
     </div>
     <div class="container" id="first-carousel">
-        <div class="container-img"><img src="img/Boucherie (3).jpg" alt="boucherie" /></div>
+        <div class="container-img"><img src="img/IMG_20240411_144216.jpg" alt="boucherie" /></div>
     </div>
     <div id="gotoshopbtn">
         <a href="boutique.html">Accès boutique</a>
     </div>
     <div class="container" id="second-carousel">
-        <div class="container-img"><img src="img/Boucherie (2).jpg" alt="boucher" /></div>
+        <div class="container-img"><img src="img/IMG_20240411_144159.jpg" alt="boucher" /></div>
     </div>
 </div>
 <div id="contain-arrowup">
